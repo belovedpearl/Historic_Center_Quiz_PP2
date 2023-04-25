9f0c0d09afc7bf22f634b4414519931f30b54e12
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2023-04-25 12:32:02 +0000

    Tidy up the script, removing the console.loggs used in testing and adding missing columns

--- assets/js/script.js
+++ assets/js/script.js
@@ -103,62 +103,64 @@ let currentQuestion = null;
 let questioncounter = 0;
 
 
-// after the document is loaded, add event listeners to all the required areas
+// DOMContent loaded allows the document to be loaded before adding event listeners to all the required areas
 
 document.addEventListener("DOMContentLoaded", function(){
+    
     // To access the game instruction, hide the firstpage and show the second one
     let instruct = document.getElementById("instruct");
     instruct.addEventListener("click", function(){
     document.getElementById("firstPage").classList.add("hide"); 
     document.getElementById("howTo").classList.remove("hide");
     })
+
     // To access the name area, hide the current page and show the next one
     let nameArea = document.getElementById("enterName");
     nameArea.addEventListener("click", function(){
-        document.getElementById("firstPage").classList.add("hide"); 
-        document.getElementById("nameArea").classList.remove("hide"); 
+    document.getElementById("firstPage").classList.add("hide"); 
+    document.getElementById("nameArea").classList.remove("hide"); 
     })
 
     // To open up the score page
     let viewScore = document.getElementById("viewScore");
-    viewScore.addEventListener("click", viewSavedScore)
+    viewScore.addEventListener("click", viewSavedScore);
     
     // To go back to the firstpage
     let back = document.getElementById("backFromInstruct");
     back.addEventListener("click", function(){
-        document.getElementById("firstPage").classList.remove("hide"); 
-        document.getElementById("howTo").classList.add("hide"); 
+    document.getElementById("firstPage").classList.remove("hide"); 
+    document.getElementById("howTo").classList.add("hide"); 
     })
+
     // To submit name value to access the game
     let submit = document.getElementById("submit");
     submit.addEventListener("click", function(){
-        let name = document.getElementById("nameValue").value;
-        document.getElementById("nameArea").classList.add("hide");
-         let welcomePage = document.getElementById("welcome");
-         welcomePage.classList.remove("hide");
-        welcome(name) 
+    let name = document.getElementById("nameValue").value;
+    document.getElementById("nameArea").classList.add("hide");
+    let welcomePage = document.getElementById("welcome");
+    welcomePage.classList.remove("hide");
+    welcome(name); 
     })
     
-     // To access the main game area
+    // To access the main game area
     let startGame = document.getElementById("gameStart");
     startGame.addEventListener("click", function(){
-        let gameStartArea = document.getElementById("game");
+    let gameStartArea = document.getElementById("game");
     gameStartArea.classList.remove("hide");
-     let welcomePage = document.getElementById("welcome");
-     welcomePage.classList.add("hide");
+    let welcomePage = document.getElementById("welcome");
+    welcomePage.classList.add("hide");
 
-     // To return back to the start game listen for
-     let reStart = document.getElementById("restart")
-     reStart.addEventListener("click", startOver)
+    // To return back to the start game listen for
+    let reStart = document.getElementById("restart");
+    reStart.addEventListener("click", startOver);
 
     // Add event listener to save score
-    let save = document.getElementById("save")
-     save.addEventListener("click", saveHighScore)
+    let save = document.getElementById("save");
+    save.addEventListener("click", saveHighScore);
 
-
-     // call functions to continue running the game
-     runGame()
-     countdown()
+    // call functions to continue running the game
+    runGame();
+    countdown();
     })
     
     
@@ -166,31 +168,31 @@ document.addEventListener("DOMContentLoaded", function(){
 
 
 /**
- * Capture the username in a welcome greeting
- * Takes its value from the input given in the page before
- */
+* Capture the username in a welcome greeting
+* Takes its value from the input given in the page before
+*/
 function welcome(name){
-   // console.log(name)
     let playerName = document.getElementById("playerName");
-    playerName.innerText = name
+    playerName.innerText = name;
 }
 
 
 /**
- * Gets a random question
- * Sets it on display to the user
- */
+* Gets a random question
+* Sets it on display to the user
+*/
 function runGame(){
-    resetOptions()
+    resetOptions();
     let questionNumber = document.getElementById("questionNum");
     questionNumber.innerText = questioncounter++;
-   //console.log(questioncounter)
+   
     if (questioncounter > 10){
-        stopGame()
-        }else if(questioncounter <= 10){
+        stopGame();
+    } else if (questioncounter <= 10){
         const questionIndex = Math.floor(Math.random() * 5);
+        // Use the question index to access the question from the array
         currentQuestion = questions[questionIndex];
-
+        // Display the question and option using the index method
         let image = document.getElementById("questionImage");
         image.src = currentQuestion.question;
         
@@ -203,57 +205,58 @@ function runGame(){
         let choice4 = document.getElementById("option4");
         choice4.innerText = currentQuestion.options[3];
         // Splice is used to remove the current question from the available questions to avoid repetition of questions
-        questions.splice(questionIndex, 1) 
+        questions.splice(questionIndex, 1) ;
     }
     
 }
 
 /**
- *  Count down to the required time
- * At endtime; calls the function to end the game
- */
+*  Count down to the required time
+* At endtime; calls the function to end the game
+*/
 function countdown(){
-    let counter = 360;
-     setInterval(()=>{
-     let time = document.getElementById("time");
-     time.innerText = counter--;
-      if(counter === 0){
-          time.innerText = "Time Up!!!"
-          stopGame()
-      }
-     }, 1000)
+    let counter = 60;
+    setInterval(()=>{
+    let time = document.getElementById("time");
+    time.innerText = counter--;
+    if(counter === 0){
+        time.innerText = "Time Up!!!"
+        stopGame();
+    }
+    }, 1000);
      
   }
+
 
 // Check the options buttons with their class name for a click
 // Add the class correct, increase score and add to the progress bar if selected one is correct
 // Add the class wrong, increase wrong if selected one is wrong
 // Disable the button after the first click
-  let options = document.getElementsByClassName("option")
+let options = document.getElementsByClassName("option");
 for (let option of options){
     option.addEventListener("click", e => {
         let selectAns = e.target;
-        //console.log(selectAns);
+        
         if (selectAns.innerText === currentQuestion.answer){
             option.classList.add("correct"); 
-            increaseScore()
-            progressBar()
+            increaseScore();
+            progressBar();
         
-        }else{
-            option.classList.add("wrong")
-            increaseWrong()
+        } else {
+            option.classList.add("wrong");
+            increaseWrong();
     } 
-     disableClick();
-        setTimeout(runGame, 1000);
-})}
+    disableClick();
+    setTimeout(runGame, 1000);
+})
+}
 
 /**
- * Disable further clicks on answer buttons 
- */
-
+* Disable further clicks on answer buttons 
+*/
 function disableClick(){
     let options = document.getElementsByClassName("option");
-    //console.log(options)
+    
     for (let option of options){
         option.disabled = true;
     }
@@ -261,48 +264,50 @@ function disableClick(){
 
 
 /**
- * Get current score from the DOM and increase it by 1
- */
- function increaseScore(){
+* Get current score from the DOM and increase it by 1
+*/
+function increaseScore(){
     let score = document.getElementById("points").innerText;
     document.getElementById('points').innerText = ++score;
 }
 
+
 /**
- * Get current count from the DOM and increase it by 1
- */
+* Get current count from the DOM and increase it by 1
+*/
  function increaseWrong(){
     let wrong = document.getElementById("wrong").innerText;
     document.getElementById('wrong').innerText = ++wrong;
 }
 
+
 /**
- * Stops the game
- * Brings up the hidden section
- * Set the score and right answer in the string
- */
- function stopGame(){
+* Stops the game
+* Brings up the hidden section
+* Set the score and right answer in the string
+*/
+function stopGame(){
     setTimeout(function (){
         let restart = document.getElementById("restart");
-        restart.focus()
+        restart.focus();
     }, 360)
 
- document.body.classList.add("overlay")
- let score = document.getElementById("points").innerText;
- let name = document.getElementById("nameValue").value;
- //let wrong = document.getElementById("wrong").innerText;
- let userName = document.getElementById("username")
-    userName.innerText = name
- let rightAns = document.getElementById("rightAns");
+    document.body.classList.add("overlay");
+    let score = document.getElementById("points").innerText;
+    let name = document.getElementById("nameValue").value;
+    
+    let userName = document.getElementById("username");
+    userName.innerText = name;
+    let rightAns = document.getElementById("rightAns");
     rightAns.innerText = score;
- let totalScore = document.getElementById("totalScore");
+    let totalScore = document.getElementById("totalScore");
     totalScore.innerText = score * 10;
 } 
 
  
 /**
- * Resets the state of the question box
- */
+* Resets the state of the question box
+*/
 function resetOptions(){
     let options = document.getElementsByClassName("option");
     for (let option of options){
@@ -312,104 +317,99 @@ function resetOptions(){
     }
 }
 
-/**
- * Regulate progress bar increase as more right answer are provided
- */
 
+/**
+* Regulate progress bar increase as more right answer are provided
+*/
 function progressBar(){
     let score = document.getElementById("points").innerText;
     
     let progressBar = document.querySelector(".progress-inner");
-    progressBar.style.transform = `scaleX(${score/10})`
- }
+    progressBar.style.transform = `scaleX(${score/10})`;
+}
 
 
- /**
-  * Takes the game back to the beginning for use by another user
-  */
-
-  function startOver(){
+/*
+* Takes the game back to the beginning for use by another user
+*/
+function startOver(){
     document.body.classList.remove("overlay");
     questioncounter = 0;
     let score = document.getElementById("points").innerText;
     score.innerText = 0;
     let wrong = document.getElementById("wrong").innerText;
     wrong.innerText = 0;
-    window.location.reload()
- }
+    window.location.reload();
+}
 
 
- /**
-  * Opens up the high score page
-   * Gets the saved score from the local storage
-   * List the saved scores
- */
- function viewSavedScore(){
-   document.getElementById("firstPage").classList.add("hide"); 
-   document.getElementById("highScore").classList.remove("hide");
-
-// Gets saved item from the local storage andputs it on the highscore page
-let highScorePage = JSON.parse(localStorage.getItem("scores") || [])
-console.log(highScorePage)
-let highContainer = document.getElementById("highScoreContainer");
-
-highContainer.innerHTML = highScorePage.map(scores => {
-return `
-    <li class="high-score"> ${scores.nameX} - ${scores.scoreX}</li>
-       `
-}).join('');
- }
-
-
- /**
-  *  Open up the high score page
-  */
- function saveScore(){
-    // selects the different sections with the class hide
-    document.getElementById("firstPage").classList.add("hide")
-    document.getElementById("highScore").classList.remove("hide")
+/**
+* Opens up the high score page
+* Gets the saved score from the local storage
+* List the saved scores
+*/
+function viewSavedScore(){
+    document.getElementById("firstPage").classList.add("hide"); 
+    document.getElementById("highScore").classList.remove("hide");
 
     // Gets saved item from the local storage andputs it on the highscore page
-    let highScorePage = JSON.parse(localStorage.getItem("scores") || [])
-    console.log(highScorePage)
+    let highScorePage = JSON.parse(localStorage.getItem("scores") || []);
+    let highContainer = document.getElementById("highScoreContainer");
+
+    highContainer.innerHTML = highScorePage.map(scores => {
+    return `
+        <li class="high-score"> ${scores.nameX} - ${scores.scoreX}</li>
+          `
+    }).join('');
+}
+
+
+/**
+*  Open up the high score page
+*/
+function saveScore(){
+    // selects the different sections with the class hide
+    document.getElementById("firstPage").classList.add("hide");
+    document.getElementById("highScore").classList.remove("hide");
+
+    // Gets saved item from the local storage andputs it on the highscore page
+    let highScorePage = JSON.parse(localStorage.getItem("scores") || []);
+    console.log(highScorePage);
     let highContainer = document.getElementById("highScoreContainer");
   
     highContainer.innerHTML = highScorePage.map(scores => {
     return `
         <li class="high-score"> ${scores.nameX} - ${scores.scoreX}</li>
-           `
-  }).join('');
- }
+           `;
+    }).join('');
+}
 
 
- /**
-  * Return the page back to the home page
-  */
- function highBackHome(){
-    document.getElementById("firstPage").classList.remove("hide")
-    document.getElementById("highScore").classList.add("hide")
- }
+/**
+* Return the page back to the home page
+*/
+function highBackHome(){
+    document.getElementById("firstPage").classList.remove("hide");
+    document.getElementById("highScore").classList.add("hide");
+}
 
- /**
-  * Save score
-  * Resolve player name and score into an object
-  * Sets and save to the local storage
-  */
- function saveHighScore(){
-    let score = document.getElementById("totalScore")
-    console.log(score.innerText)
-    let name = document.getElementById("playerName")
-    console.log(name.innerText)
- 
+/**
+* Save score
+* Resolve player name and score into an object
+* Sets and save to the local storage
+*/
+function saveHighScore(){
+    let score = document.getElementById("totalScore");
+    let name = document.getElementById("playerName");
+
     let highScore = {
         scoreX : score.innerText,
         nameX : name.innerText
     }
 
-    let getScore = JSON.parse(localStorage.getItem("scores")) || []
-    getScore.push(highScore)
-    getScore.sort(function(a, b){return b.scoreX - a.scoreX})
-    getScore.splice(5)
-    console.log(getScore)
-    localStorage.setItem("scores", JSON.stringify(getScore))
+    let getScore = JSON.parse(localStorage.getItem("scores")) || [];
+    getScore.push(highScore);
+    getScore.sort(function(a, b){return b.scoreX - a.scoreX});
+    getScore.splice(5);
+    localStorage.setItem("scores", JSON.stringify(getScore));
 }
